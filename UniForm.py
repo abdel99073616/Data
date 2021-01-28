8020f40a1e7e0f98939cb8913621333ae07b7452
Abdo Mohamed <abdel99073616@gmail.com>
2021-01-28 04:57:02 +0200

    3

--- UniForm.py
+++ UniForm.py
@@ -1,9 +1,15 @@
 from numpy import random
+from pandas import DataFrame
 
 x = random.uniform(size=(2, 3))
 
-s = random.uniform(60,120,size=(1000,19))
+s = random.uniform(60,120,size=(3000,19))
 
+data = [list( map(int,i) ) for i in s]
 
-print ( [list( map(int,i) ) for i in s] )
+df = DataFrame (data,columns=['Calculus','DataBase','LinerAlgebra','Intro_to_CS','Intro_to_IS',
+                  'Discrete_Math','ObjectOriented','Statistics','ProgramingLanguage',
+                  'DifferentialEquation','Operations_Researsh','DataStructure','FileProcessing','AdvancedMathematics',
+                  'Physics','Stochastic','Multimedia','InformationTheory','SystemAnalysis_And_Design'])
 
+df.to_csv(r'/home/abdo/Data/NewData.csv', index = False)
